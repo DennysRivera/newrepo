e4f4995edcd052e23b105eb81bce6fe43fb1a2f7
DennysRivera <00026919@uca.edu.sv>
2025-05-06 22:16:17 +0000

    My commit

---
+++ index.js
@@ -0,0 +1,39 @@
+import express from 'express';
+const app = express();
+
+app.get('/', async (req, res) => {
+    let numbers = []
+    for(let i = 0; i < 5; i++){
+        numbers.push(test())
+    }
+    
+
+    res.send(`Hello World ${numbers}`)
+    
+});
+
+app.get('/hello', async (req, res) => {
+  console.log("Hello World")
+  res.send("Hello World!")
+});
+
+function test(){
+  let number = Math.random() * 20
+  console.log(number)
+  return number
+}
+
+app.get('/nono', (req, res) => {
+  res.status(403).send({msg: "This is a JSON response"})
+  
+})
+
+app.get('/endpoint', (req, res) => {
+  res.status(403).send({msg: "This is a JSON response"})
+  
+})
+
+const port = parseInt(process.env.PORT) || 8080;
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
